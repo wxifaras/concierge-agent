7e99497f576c0e2ddf9450cf83462972f323918b
William Xifaras <wxifaras@outlook.com>
2025-01-22 08:23:28 -0500

    models

---
+++ concierge-agent-api/Models/LotLocation.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace concierge_agent_api.Models
+{
+    public class LotLocation
+    {
+        public string actual_lot { get; set; }
+        public double lat { get; set; }
+        [JsonProperty("long")]
+        public double longitude { get; set; }
+        public string locationType { get; set; }
+    }
+}
