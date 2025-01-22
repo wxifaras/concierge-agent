fb71a9385c31a122112a5697c1e12452ff38a836
William Xifaras <wxifaras@outlook.com>
2025-01-22 09:34:12 -0500

    added azure storage options

--- concierge-agent-api/Models/AzureStorageOptions.cs
+++ concierge-agent-api/Models/AzureStorageOptions.cs
@@ -2,6 +2,8 @@
 {
     public class AzureStorageOptions
     {
+        public const string AzureStorage = "AzureStorageOptions";
+
         public string ConnectionString { get; set; }
         public string QueueName { get; set; }
     }
