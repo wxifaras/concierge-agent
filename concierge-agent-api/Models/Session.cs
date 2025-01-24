8d64dbd05fd2599b9f870ea4a13db298be43ca1c
William Xifaras <wxifaras@outlook.com>
2025-01-24 11:25:53 -0500

    cosmosdb chat  history provider

---
+++ concierge-agent-api/Models/Session.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+
+namespace concierge_agent_api.Models;
+
+public record Session
+{
+    /// <summary>
+    /// Unique identifier
+    /// </summary>
+    [JsonProperty(PropertyName = "id")]
+    public string Id { get; set; }
+
+    public string Type { get; set; }
+
+    public string SmsNumber { get; set; }
+
+    public string SessionId { get; set; }
+
+    public string UserId { get; set; }
+
+    public string? Name { get; set; }
+
+    public DateTime Timestamp { get; set; }
+
+    [JsonIgnore]
+    public List<Message> Messages { get; set; }
+
+    public void AddMessage(Message message)
+    {
+        Messages.Add(message);
+    }
+
+    public void UpdateMessage(Message message)
+    {
+        var match = Messages.Single(m => m.Id == message.Id);
+        var index = Messages.IndexOf(match);
+        Messages[index] = message;
+    }
+}
---
+++ concierge-agent-api/Services/CosmosDbChatHistoryManager.cs
@@ -0,0 +1,23 @@
+﻿using concierge_agent_api.Models;
+
+namespace concierge_agent_api.Services;
+
+public interface ICosmosDbChatHistoryManager
+{
+    Session GetOrCreateChatHistoryAsync(string sessionId);
+}
+
+public class CosmosDbChatHistoryManager : ICosmosDbChatHistoryManager
+{
+    private readonly ICosmosDbService _cosmosDbService;
+
+    public CosmosDbChatHistoryManager(ICosmosDbService cosmosDbService)
+    {
+        _cosmosDbService = cosmosDbService;
+    }
+
+    public Session GetOrCreateChatHistoryAsync(string sessionId)
+    {
+        throw new NotImplementedException();
+    }
+}
