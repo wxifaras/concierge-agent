812caae173419d4b406e3292ccc26cbd75d3ff6b
William Xifaras <wxifaras@outlook.com>
2025-01-28 13:05:34 -0500

    updated LotLookup

--- concierge-agent-api/Models/LotLookup.cs
+++ concierge-agent-api/Models/LotLookup.cs
@@ -4,8 +4,8 @@ public class LotLookup
 {
     public string lot_name { get; set; }
     public string actual_lot { get; set; }
-    public DateTime first_event { get; set; }
-    public DateTime last_event { get; set; }
-    public DateTime ETL_Created_DTM { get; set; }
-    public DateTime ETL_Updated_DTM { get; set; }
+    public DateTime? first_event { get; set; }
+    public DateTime? last_event { get; set; }
+    public DateTime? ETL_Created_DTM { get; set; }
+    public DateTime? ETL_Updated_DTM { get; set; }
 }
