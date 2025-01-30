05744fab05239f79c6541fa2ed2912f3794b669f
William Xifaras <wxifaras@outlook.com>
2025-01-30 13:47:53 -0500

    event plugin changes

--- concierge-agent-api/Plugins/DirectionsPlugin.cs
+++ concierge-agent-api/Plugins/DirectionsPlugin.cs
@@ -1,11 +1,8 @@
-﻿using Asp.Versioning.Conventions;
-using concierge_agent_api.Models;
+﻿using concierge_agent_api.Models;
 using concierge_agent_api.Services;
 using Microsoft.Extensions.Caching.Memory;
-using Microsoft.Extensions.Logging;
 using Microsoft.SemanticKernel;
 using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
 using System.ComponentModel;
 
 namespace concierge_agent_api.Plugins;
@@ -39,8 +36,7 @@ public class DirectionsPlugin
         [Description("The address of the destination")] string destinationAddress,
         [Description("The latitude of the destination")] string destinationLatitude,
         [Description("The longitude of the destination")] string destinationLongitude,
-        [Description("The travel mode to the stadium")] TravelMode travelMode
-        )
+        [Description("The travel mode to the stadium")] TravelMode travelMode)
     {
         _logger.LogInformation($"get_distance_to_destination");
 
@@ -58,8 +54,7 @@ public class DirectionsPlugin
         [Description("The longitude of the origin")] string originLongitude,
         [Description("The address of the destination")] string destinationAddress,
         [Description("The latitude of the destination")] string destinationLatitude,
-        [Description("The longitude of destination")] string destinationLongitude
-        )
+        [Description("The longitude of destination")] string destinationLongitude)
     {
         _logger.LogInformation($"get_directions_to_destination");
 
@@ -80,32 +75,37 @@ public class DirectionsPlugin
     {
         _logger.LogInformation($"get_parking_options");
         List<LotLocation> lotLocations = _memoryCache.Get<List<LotLocation>>($"LotLocations-{tmEventId}");
+        var enrichedJson = string.Empty;
 
-        if (!_memoryCache.TryGetValue("EnrichedLotLocations", out List<EnrichedLotLocation> enrichedLotLocations))
+        if (lotLocations != null)
         {
-            enrichedLotLocations = new List<EnrichedLotLocation>();
-            // based on whether the customer is open to a short walk or not, which parking recommendations to provide
-            foreach (LotLocation lotLocation in lotLocations)
+            if (!_memoryCache.TryGetValue("EnrichedLotLocations", out List<EnrichedLotLocation> enrichedLotLocations))
             {
-                int distanceToStadium = await _azureMapsService.GetDistanceAsync(lotLocation.lat, lotLocation.longitude, double.Parse(destinationLatitude), double.Parse(destinationLongitude), TravelMode.pedestrian);
-
-                var enrichedLotLocation = new EnrichedLotLocation
+                enrichedLotLocations = new List<EnrichedLotLocation>();
+                // based on whether the customer is open to a short walk or not, which parking recommendations to provide
+                foreach (LotLocation lotLocation in lotLocations)
                 {
-                    lot_lat = lotLocation.lat.ToString(),
-                    lot_long = lotLocation.longitude.ToString(),
-                    actual_lot = lotLocation.actual_lot,
-                    location_type = lotLocation.locationType,
-                    distance_to_stadium_in_meters = distanceToStadium.ToString(),
-                    lot_price = lotLocation.lot_price
-                };
+                    int distanceToStadium = await _azureMapsService.GetDistanceAsync(lotLocation.lat, lotLocation.longitude, double.Parse(destinationLatitude), double.Parse(destinationLongitude), TravelMode.pedestrian);
 
-                enrichedLotLocations.Add(enrichedLotLocation);
+                    var enrichedLotLocation = new EnrichedLotLocation
+                    {
+                        lot_lat = lotLocation.lat.ToString(),
+                        lot_long = lotLocation.longitude.ToString(),
+                        actual_lot = lotLocation.actual_lot,
+                        location_type = lotLocation.locationType,
+                        distance_to_stadium_in_meters = distanceToStadium.ToString(),
+                        lot_price = lotLocation.lot_price
+                    };
+
+                    enrichedLotLocations.Add(enrichedLotLocation);
+                }
+
+                _memoryCache.Set("EnrichedLotLocations", enrichedLotLocations, TimeSpan.FromMinutes(120));
             }
 
-            _memoryCache.Set("EnrichedLotLocations", enrichedLotLocations, TimeSpan.FromMinutes(120));
+            enrichedJson = JsonConvert.SerializeObject(enrichedLotLocations, Formatting.Indented);
         }
-
-        var enrichedJson = JsonConvert.SerializeObject(enrichedLotLocations, Formatting.Indented);
+        
         return enrichedJson;
     }
 
@@ -116,8 +116,7 @@ public class DirectionsPlugin
         [Description("The address of the origin")] string originAddress,
         [Description("The latitude of the origin")] string originLatitude,
         [Description("The longitude of the origin")] string originLongitude,
-        [Description("JSON list of MARTA stations closest to the customer")] string jsonMartaStations
-        )
+        [Description("JSON list of MARTA stations closest to the customer")] string jsonMartaStations)
     {
         _logger.LogInformation($"get_closest_marta_station");
 
@@ -126,15 +125,15 @@ public class DirectionsPlugin
         // add the distance to each MARTA station from the customer's origin
         foreach (var station in stationList)
         {
-            string stationLat = (string)station["station_lat"];
-            string stationLong = (string)station["station_long"];
+            var stationLat = (string)station["station_lat"];
+            var stationLong = (string)station["station_long"];
 
             int distanceToStation = await _azureMapsService.GetDistanceAsync(double.Parse(originLatitude), double.Parse(originLongitude), double.Parse(stationLat), double.Parse(stationLong), TravelMode.car);
 
             station["distanceToStation"] = distanceToStation;
         }
 
-        string strStationList = JsonConvert.SerializeObject(stationList, Formatting.Indented);
+        var strStationList = JsonConvert.SerializeObject(stationList, Formatting.Indented);
 
         return strStationList;
     }
